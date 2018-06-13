693b9ad8d78890da200ee49bf2472444ad55ba1f
RGWantz <39272961+RGWantz@users.noreply.github.com>
2018-06-13 10:51:34 -0400

    Update environment.ts

--- week4/redux-demo/src/environment.ts
+++ week4/redux-demo/src/environment.ts
@@ -3,7 +3,8 @@ const dev = {
 }
 
 const prod = {
-  context: 'http://ec2-18-237-118-203.us-west-2.compute.amazonaws.com:3001/'
+  context: 'ec2-54-167-136-172.compute-1.amazonaws.com:3001/'
+  
 }
 
 export const environment = process.env.NODE_ENV === 'production'
